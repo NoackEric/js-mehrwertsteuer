c906874f4b3653e373c006a218e012f004adf996
Eric Noack <ericnoack91@gmail.com>
2019-10-31 23:10:21 +0100

    division_noch_fehlerhaft

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ function calc() {
     let multiplikator = document.getElementById("netto");    // operator *
     var x = document.getElementById("groß").checked ? .19 : .07;
     var steuer = ''
-    
+
     if (multiplikator.checked) {
         steuer = betrag * x;
     } else {
         steuer = betrag / (1 + x);
     }
-       var endsumme = Number(steuer) + Number(betrag)  
-     console.log(steuer)
-document.getElementById("müll").innerHTML = `<p> ${Number(steuer.toFixed(2))} €</p>`;
-document.getElementById("müll2").innerHTML = `<p> ${Number(endsumme)} €</p>`;
+    var endsumme = Number(steuer) + Number(betrag)
+    console.log(steuer)
+    document.getElementById("müll").innerHTML = `<p> ${Number(steuer.toFixed(2))} €</p>`;
+    document.getElementById("müll2").innerHTML = `<p> ${Number(endsumme)} €</p>`;
 }
