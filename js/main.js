ee16dffa60cd24c038908d438514ecff96fd577d
Eric Noack <ericnoack91@gmail.com>
2019-10-31 23:17:11 +0100

    fehler_behoben

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ function calc() {
     let multiplikator = document.getElementById("netto");    // operator *
     var x = document.getElementById("groß").checked ? .19 : .07;
     var steuer = ''
-
+    
     if (multiplikator.checked) {
         steuer = betrag * x;
+        var endsumme = Number(steuer) + Number(betrag)
     } else {
         steuer = betrag / (1 + x);
+        var endsumme = Number(betrag) - Number(steuer)
     }
-    var endsumme = Number(steuer) + Number(betrag)
+    
     console.log(steuer)
     document.getElementById("müll").innerHTML = `<p> ${Number(steuer.toFixed(2))} €</p>`;
-    document.getElementById("müll2").innerHTML = `<p> ${Number(endsumme)} €</p>`;
+    document.getElementById("müll2").innerHTML = `<p> ${Number(endsumme.toFixed(2))} €</p>`;
 }
