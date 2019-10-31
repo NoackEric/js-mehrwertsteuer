a59a62b245519b21babefa92667d96c4a27e4c70
Eric Noack <ericnoack91@gmail.com>
2019-10-31 22:18:18 +0100

    mehrwertsteuer_ausgabe

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ function calc() {
     var betrag = document.getElementById("betrag").value;   // eingegebener Wert (ausgangswert)
     let multiplikator = document.getElementById("netto");    // operator *
     var x = document.getElementById("groß").checked ? .19 : .07;
-        
+    var steuer = ''
 
     if (multiplikator.checked) {
-        betrag = betrag * x;
+        steuer = betrag * x;
     } else {
-        betrag = betrag / (1 + x);
+        steuer = betrag / (1 + x);
     }
         
-     console.log(betrag)
-
+     console.log(steuer)
+document.getElementById("müll").innerHTML = `${steuer}`;
 }
-    
+
 
 
 
